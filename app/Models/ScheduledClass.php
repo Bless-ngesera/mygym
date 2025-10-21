523617a3d39c4ca945b90c6d7c97c8ba2d6badd9
Bless-ngesera <164491766+Bless-ngesera@users.noreply.github.com>
2025-10-21 22:10:07 +0300

    Recreate ScheduleClass.php

---
+++ app/Models/ScheduledClass.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ScheduledClass extends Model
+{
+    //
+    use HasFactory;
+
+    protected $guarded = null;
+
+    protected $casts = [
+        'date_time' => 'datetime'
+    ];
+
+    public function instructor(){
+        return $this->belongsTo(User::class, 'instructor_id');
+    }
+
+    public function classType(){
+        return $this->belongsTo(ClassType::class);
+    }
+}
