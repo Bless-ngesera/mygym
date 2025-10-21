f04235e069fbde4000ace882dd546d66fa796622
Bless-ngesera <164491766+Bless-ngesera@users.noreply.github.com>
2025-10-21 22:09:13 +0300

    Update ClassType.php
    Correct a typo on the code

--- app/Models/ClassType.php
+++ app/Models/ClassType.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ClassType extends Model
 {
     //
-    use HasFactpory;
+    use HasFactory;
 
     public function scheduledClasses(){
         return $this->hasMany(ScheduledClass::class);
