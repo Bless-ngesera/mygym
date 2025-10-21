081b89d5b3283c0e613bca743e3a11e237494bfc
Bless-ngesera <164491766+Bless-ngesera@users.noreply.github.com>
2025-10-21 22:31:09 +0300

    Update Navigation.php
    Adding the Schedule a class and Upcoming Classes links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,14 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('schedule.create')" :active="request()->routeIs('schedule.create')">
+                        Schedule a class
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('schedule.index')" :active="request()->routeIs('schedule.index')">
+                        Upcoming Classes
+                    </x-nav-link>
                 </div>
             </div>
 
